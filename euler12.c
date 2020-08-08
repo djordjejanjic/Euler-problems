bccda9a83b2e67de717e0c3cb87c0e4887fb545a
djord <djord@DESKTOP-EA378QQ>
2020-08-09 01:51:34 +0200

    Problem 12 - needs maybe 2 minutes to produce the result

---
+++ euler12.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(){
+
+	unsigned int sum = 0;
+	int brojac = 1;
+	int flag = 0;
+	
+	for(unsigned int i = 10000; i < 100000; i++){
+		for(unsigned int j = 1; j <= i; j++){
+			sum += j;
+		}
+		printf("%d - ", sum);
+		for(unsigned int k = 1; k <= sum/2; k++){
+			if(sum % k == 0){
+				brojac++;
+				if(brojac > 500){
+					break;
+				}
+			}
+		}
+		if(brojac >= 500){
+			printf("REZULTAT: %d\n", sum);
+			break;
+		}
+		else{
+			printf("%d\n", brojac);
+			brojac = 0;
+		}
+		sum = 0;
+	}
+
+	return 0;
+}
